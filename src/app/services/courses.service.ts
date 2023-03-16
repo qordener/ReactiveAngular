719aab0230e1fd1a7b83285db57cdf248de9e833
Quentin Ordener <quentin.ordener@web-atrio.com>
2023-03-16 16:08:40 +0100

    Single Data pattern

--- src/app/services/courses.service.ts
+++ src/app/services/courses.service.ts
@@ -18,6 +18,23 @@ export class CoursesService {
     );
   }
 
+  loadCourseById(courseId: number): Observable<Course> {
+    return this.http
+      .get<Course>(`/api/courses/${courseId}`)
+      .pipe(shareReplay());
+  }
+
+  loadAllCourseLessons(courseId: number): Observable<Lesson[]> {
+    return this.http
+      .get<Lesson[]>('/api/lessons/', {
+        params: { courseId: courseId.toString(), pageSize: '10000' },
+      })
+      .pipe(
+        map((res) => res['payload']),
+        shareReplay()
+      );
+  }
+
   saveCourse(courseId: string, changes: Partial<Course>): Observable<any> {
     return this.http
       .put(`/api/courses/${courseId}`, changes)
