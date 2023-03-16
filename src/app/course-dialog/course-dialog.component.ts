0c961e2b7c6bab635d86ec42b01447f04ba9a89f
Quentin Ordener <quentin.ordener@web-atrio.com>
2023-03-16 15:02:46 +0100

    Implement Store

--- src/app/course-dialog/course-dialog.component.ts
+++ src/app/course-dialog/course-dialog.component.ts
@@ -16,12 +16,13 @@ import { throwError } from 'rxjs';
 import { CoursesService } from '../services/courses.service';
 import { LoadingService } from '../loading/loading.service';
 import { MessagesService } from '../messages/messages.service';
+import { CoursesStore } from '../services/courses.store';
 
 @Component({
   selector: 'course-dialog',
   templateUrl: './course-dialog.component.html',
   styleUrls: ['./course-dialog.component.css'],
-  providers: [LoadingService, MessagesService],
+  providers: [MessagesService],
 })
 export class CourseDialogComponent implements AfterViewInit {
   form: FormGroup;
@@ -32,8 +33,7 @@ export class CourseDialogComponent implements AfterViewInit {
     private fb: FormBuilder,
     private dialogRef: MatDialogRef<CourseDialogComponent>,
     @Inject(MAT_DIALOG_DATA) course: Course,
-    private coursesService: CoursesService,
-    private loadingService: LoadingService,
+    private coursesStore: CoursesStore,
     private messagesService: MessagesService
   ) {
     this.course = course;
@@ -51,21 +51,9 @@ export class CourseDialogComponent implements AfterViewInit {
   save() {
     const changes = this.form.value;
 
-    const saveCourse$ = this.coursesService
-      .saveCourse(this.course.id, changes)
-      .pipe(
-        catchError((err) => {
-          const message = 'Could not save course';
-          this.messagesService.showErrors(message);
-          return throwError(err);
-        })
-      );
+    this.coursesStore.saveCourse(this.course.id, changes).subscribe();
 
-    this.loadingService
-      .showLoaderUntilCompleted(saveCourse$)
-      .subscribe((val) => {
-        this.dialogRef.close(val);
-      });
+    this.dialogRef.close(changes);
   }
 
   close() {
